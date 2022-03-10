cec5253051a627d6259a2d0155f763a94ba92fa6
hollister1822 <hollister0811822@gmail.com>
2022-03-10 12:39:44 +0600

    br fixture in 'conftest.py' updated. now we can select browser for test (chrome on default or firefox)

--- conftest.py
+++ conftest.py
@@ -2,10 +2,28 @@ import pytest
 from selenium import webdriver
 
 
-@pytest.fixture(scope='function')
-def br():
-    print('\nstart browser for test...')
-    br = webdriver.Chrome()
+supported_browsers = {
+    'chrome': webdriver.Chrome,
+    'firefox': webdriver.Firefox
+}
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def br(request):
+    browser_name = request.config.getoption("browser_name")
+
+    if browser_name in supported_browsers:
+        br = supported_browsers.get(browser_name)()
+        print(f"\nstart {browser_name} browser for test..")
+    else:
+        joined_browsers = ', '.join(supported_browsers.keys())
+        raise pytest.UsageError(f"--browser_name is invalid, supported browsers: {joined_browsers}")
+
     yield br
-    print('\nquit browser for test...')
+    print("\nquit browser..")
     br.quit()
